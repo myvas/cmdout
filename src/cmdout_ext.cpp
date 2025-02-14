016034adf3ab5ec851f7903b9422b78bed277fdb
FrankH <4848285@qq.com>
2025-02-14 21:27:14 +0800

    .

--- src/cmdout_ext.cpp
+++ src/cmdout_ext.cpp
@@ -11,10 +11,10 @@ namespace myvas {
 
 std::ostream &operator<<(std::ostream &os, const myvas::cmdout &value)
 {
-    os << "cmdout: " << value.cmd() << "\n(return " << value.status() << " length " << value.out().length() << ")\n"
-       << std::string(80, '-') << "\n"
-       << value.out();
-    return os;
+	os << "cmdout: " << value.cmd() << "\n(return " << value.status() << " length " << value.out().length() << ")\n"
+	   << std::string(80, '-') << "\n"
+	   << value.out();
+	return os;
 }
 
 } // namespace myvas
